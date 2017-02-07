a5e49d92b010523521712d49e4568819db245e0f
berniey <berniey@163.com>
2017-02-07 22:31:42 +0800

    change password and email

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -32,3 +32,42 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, field):
         if WebUser.query.filter_by(username=field.data).first():
             raise ValidationError(u'用户名已存在')
+
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm new password', validators=[DataRequired()])
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('New Password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Reset Password')
+
+    def validate_email(self, field):
+        if WebUser.query.filter_by(email=field.data).first() is None:
+            raise ValidationError('Unknown email address.')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if WebUser.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
--- application/main/views.py
+++ application/main/views.py
@@ -11,20 +11,20 @@ from datetime import datetime
 #注册的蓝本名main
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = WebUser.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = WebUser(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
+    # form = NameForm()
+    # if form.validate_on_submit():
+    #     user = WebUser.query.filter_by(username=form.name.data).first()
+    #     if user is None:
+    #         user = WebUser(username=form.name.data)
+    #         db.session.add(user)
+    #         session['known'] = False
+    #         if current_app.config['FLASKY_ADMIN']:
+    #             send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+    #                        'mail/new_user', user=user)
+    #     else:
+    #         session['known'] = True
+    #     session['name'] = form.name.data
+    #     return redirect(url_for('.index'))
 
     a = request
     b = current_app
@@ -39,8 +39,6 @@ def index():
     print(c)
     print(d)
     print(ua)
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
+    return render_template('index.html', name=session.get('name'),
                            current_time=datetime.utcnow(),
                            known=session.get('known', False))
-
