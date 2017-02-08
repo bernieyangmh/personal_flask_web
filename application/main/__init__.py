54e3a8bbb30e0f26e51e4921f52f5f655f64bbb1
berniey <berniey@163.com>
2017-02-08 22:34:23 +0800

    change role

--- application/main/__init__.py
+++ application/main/__init__.py
@@ -6,3 +6,9 @@ main = Blueprint('main', __name__)
 #尾部导入避免循环依赖
 
 from . import views, errors
+from ..models import Permission
+
+#在模板中检查权限
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
--- application/models.py
+++ application/models.py
@@ -1,16 +1,56 @@
 # -*-  coding:utf-8 -*-
-
+from datetime import datetime
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 
+class Permission:
+    '''
+    8位权限表示
+    '''
+
+    FOLLOW = 0x01                   #关注用户
+    COMMENT = 0x02                  #发表评论
+    WRITE_ARTICLES = 0x04           #写文章
+    MODERATE_COMMENTS = 0x08        #管理评论
+    ADMINISTER = 0x80               #管理网站
+
 class Role(db.Model):
+    """
+    匿名只有阅读权限，
+    用户能发布文章，评论，关注其他用户
+    管理员具备所有权限，同时能修改其他用户权限
+    """
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('WebUser', backref='role', lazy='dynamic')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT |
+                     Permission.WRITE_ARTICLES, True),
+            'Moderator': (Permission.FOLLOW |
+                          Permission.COMMENT |
+                          Permission.WRITE_ARTICLES |
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return '<Role {}>'.format(self.name)
@@ -24,6 +64,19 @@ class WebUser(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(WebUser, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            else:
+                self.role = Role.query.filter_by(default=True).first()
 
     @property
     def password(self):
@@ -52,8 +105,38 @@ class WebUser(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def ping(self):
+        """
+        每次访问网站刷新时间
+        :return:
+        """
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
+    def can(self, permissions):
+        """
+        进行位与操作
+        判断角色权限
+        :param permissions:
+        :return: FALSE or TRUE
+        """
+        return self.role is not None and \
+            (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
 
 @login_manager.user_loader
 def load_user(user_id):
